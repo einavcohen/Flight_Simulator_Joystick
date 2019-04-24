6bdde17eb756988791fe91ce41b25b4509118f53
einavcohen <einavcohen006@gmail.com>
2019-04-24 13:29:48 +0300

    update InfoServer

--- FlightSimulator/Model/InfoServer.cs
+++ FlightSimulator/Model/InfoServer.cs
@@ -1,93 +1,4 @@
-﻿/*
-using FlightSimulator.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-
-namespace FlightSimulator.Model
-{
-    class InfoServer : BaseNotify
-    {
-        private static InfoServer infoServer = null;
-
-        private float lat;
-        private float lon;
-        private TcpListener listener;
-        public Action<float,float> changedLonLat;
-
-        public float Lat {
-
-            get { return lat;}
-
-            set {
-                lat = value;
-                NotifyPropertyChanged("Lat");
-            }
-        }
-
-        public float Lon {
-
-            get { return lon;}
-
-            set {
-                lon = value;
-                NotifyPropertyChanged("Lon");
-            }
-        }
-
-        public void Start()
-        {
-            int Port = ApplicationSettingsModel.Instance.FlightInfoPort;
-            string IP = ApplicationSettingsModel.Instance.FlightServerIP;
-
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5400);
-            listener = new TcpListener(ep);
-            Console.WriteLine("Waiting for client connections...");
-            listener.Start();
-            TcpClient client = listener.AcceptTcpClient();
-            Console.WriteLine("Client connected");
-            Thread thread = new Thread(() => {
-                try{
-                    StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
-                    string line = "";
-                    while ((line = reader.ReadLine()) != null)
-                    {
-
-                            string[] valuesStr = line.Split(',');
-                            Lon = float.Parse(valuesStr[0]);
-                            Lat = float.Parse(valuesStr[1]);
-
-                            Console.WriteLine("lon:" + Lon +" ,lat:" + Lat);
-
-                            changedLonLat?.Invoke(Lon, Lat);
-                            reader.DiscardBufferedData();
-                    }
-                }
-                catch(Exception exception){
-
-                }
-            });
-
-            //start listenning thread
-            thread.Start();
-       }                                          
-       
-        public void Stop()
-        {
-            listener.Stop();
-        }
-    }
-
-}*/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -118,48 +29,25 @@ namespace FlightSimulator.Model
 
         public double Lon
         {
-            get
-            {
-                return lon;
-            }
-            set
-            {
-                lon = value;
-                NotifyPropertyChanged("lon");
-            }
+            get {return lon;}
+            set {lon = value; NotifyPropertyChanged("lon");}
         }
 
         public double Lat
         {
-            get
-            {
-                return lat;
-            }
-            set
-            {
-                lat = value;
-                NotifyPropertyChanged("lat");
-            }
+            get {return lat;}
+            set {lat = value; NotifyPropertyChanged("lat");}
         }
 
-        // Explicit static constructor to tell C# compiler
-        // not to mark type as beforefieldinit
-        static InfoServer()
-        {
-        }
+        // static constructor in order to tell C# comp
+        // not to mark type as before init
+        static InfoServer(){ }
 
-        public InfoServer()
-        {
-            Connect();
-            HandleClient();
-        }
+        public InfoServer(){ }
 
         public static InfoServer Instance
         {
-            get
-            {
-                return instance;
-            }
+            get {return instance;}
         }
 
         public void Connect()
@@ -167,13 +55,13 @@ namespace FlightSimulator.Model
             IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5400);
             listener = new TcpListener(ep);
             listener.Start();
-            MessageBox.Show("after start");
+           // MessageBox.Show("after start");
         }
 
         public void HandleClient()
         {
             TcpClient client = listener.AcceptTcpClient();
-            MessageBox.Show("after connect");
+           // MessageBox.Show("after connect");
             Thread thread = new Thread(() => ReadFromClient(client));
             thread.Start();
             //ReadFromClient(client);
@@ -200,10 +88,10 @@ namespace FlightSimulator.Model
 
         void EditMessage(string Message)
         {
-            string[] splitwords = Message.Split(',');
-            this.valuesFromSim = splitwords;
-            Lon = Convert.ToDouble(splitwords[0]);
-            Lat = Convert.ToDouble(splitwords[1]);
+            string[] splitStr = Message.Split(',');
+            this.valuesFromSim = splitStr;
+            Lon = Convert.ToDouble(splitStr[0]);
+            Lat = Convert.ToDouble(splitStr[1]);
             MessageBox.Show(valuesFromSim[0]);
         }
 
